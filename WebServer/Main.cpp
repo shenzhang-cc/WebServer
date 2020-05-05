1203e2da50a389298a413976f9231b7861299ce1
shenzhang-cc <shenzhang_cc@163.com>
2020-05-05 18:24:04 +0800

    qwefqewvbr

--- WebServer/Main.cpp
+++ WebServer/Main.cpp
@@ -8,24 +8,25 @@
 
 
 int main(int argc, char *argv[]) {
-  int threadNum = 4;
-  int port = 80;
-  std::string logPath = "./WebServer.log";
+  int threadNum = 4;   //线程数
+  int port = 80;  // http服务器端口号为80
+  std::string logPath = "./WebServer.log";  //日志文件的保存位置
 
   // parse args
-  int opt;
+  int opt;  // 选项的处理
   const char *str = "t:l:p:";
+  // getopt()用于分析命令行参数
   while ((opt = getopt(argc, argv, str)) != -1) {
     switch (opt) {
-      case 't': {
-        threadNum = atoi(optarg);
-        break;
+      case 't': { //选项t用于设定线程的数目
+        threadNum = atoi(optarg);  // extern char* optarg, 指向当前选项的参数的指针。全局变量。
+        break;  // atoi 将字符串转换为整数
       }
       case 'l': {
         logPath = optarg;
         if (logPath.size() < 2 || optarg[0] != '/') {
           printf("logPath should start with \"/\"\n");
-          abort();
+          abort();  //终止不正常的进程
         }
         break;
       }
@@ -39,7 +40,7 @@ int main(int argc, char *argv[]) {
   }
   Logger::setLogFileName(logPath);
 // STL库在多线程上应用
-#ifndef _PTHREADS
+#ifndef _PTHREADS  // 如果没有引入这个头文件就
   LOG << "_PTHREADS is not defined !";
 #endif
   EventLoop mainLoop;
